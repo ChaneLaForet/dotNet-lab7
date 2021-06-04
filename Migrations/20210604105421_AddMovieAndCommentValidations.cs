be4bee5df7a58300e505c0d638d74409506e4c2a
ChaneLaForet <72274437+ChaneLaForet@users.noreply.github.com>
2021-06-04 13:55:01 +0300

    Add AddMovieAndCommentValidations migrations.

---
+++ Migrations/20210604105421_AddMovieAndCommentValidations.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lab2.Migrations
+{
+    public partial class AddMovieAndCommentValidations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movies",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateAdded",
+                table: "Movies",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTime",
+                table: "Comments",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_Title",
+                table: "Movies",
+                column: "Title",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_Title",
+                table: "Movies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateAdded",
+                table: "Movies",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTime",
+                table: "Comments",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Comments",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
