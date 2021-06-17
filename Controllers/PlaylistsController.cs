3dc93c2917c18ab7d5854c01b44f814efe51a5bd
ChaneLaForet <72274437+ChaneLaForet@users.noreply.github.com>
2021-06-17 12:33:01 +0300

    Add DeletePlaylist method.

--- Controllers/PlaylistsController.cs
+++ Controllers/PlaylistsController.cs
@@ -70,7 +70,7 @@ namespace Lab2.Controllers
             return Ok();
         }
 
-        // GET: https://localhost:5001/api/movielists
+        // GET: https://localhost:5001/api/playlists
         [HttpGet]
         public async Task<ActionResult<IEnumerable<PlaylistsForUserResponse>>> GetAll()
         {
@@ -88,31 +88,6 @@ namespace Lab2.Controllers
             return _mapper.Map<List<Playlist>, List<PlaylistsForUserResponse>>(result);
         }
 
-        /*
-        // GET: https://localhost:5001/api/playlists/1
-        [HttpGet("{id}")]
-        public async Task<ActionResult<PlaylistsForUserResponse>> GetMovieList(int id)
-        {
-            var user = await _userManager.FindByNameAsync(User.FindFirst(ClaimTypes.NameIdentifier).Value);
-
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            var playlist = await _context.Playlists.FindAsync(id);
-
-            if (playlist == null)
-            {
-                return NotFound();
-            }
-
-            var resultViewModel = _mapper.Map<PlaylistsForUserResponse>(playlist);
-
-            return Ok(resultViewModel);
-        }
-        */
-
         // GET: https://localhost:5001/api/playlists/1
         [HttpGet("{id}")]
         public async Task<ActionResult<PlaylistsForUserResponse>> GetPlaylistById(int id)
@@ -124,18 +99,42 @@ namespace Lab2.Controllers
                 return NotFound();
             }
 
-            var checkPlaylist = await _context.Playlists.FindAsync(id);
+            var playlist = _context.Playlists.Where(p => p.ApplicationUser.Id == user.Id && p.Id == id).Include(p => p.Movies).FirstOrDefault();
 
-            if (checkPlaylist == null)
+            if (playlist == null)
             {
                 return NotFound();
             }
-
-            var playlist = _context.Playlists.Where(p => p.ApplicationUser.Id == user.Id && p.Id == id).Include(p => p.Movies).First();
 
             var resultViewModel = _mapper.Map<Playlist, PlaylistsForUserResponse>(playlist);
 
             return Ok(resultViewModel);
         }
+
+        // DELETE: https://localhost:5001/api/playlists/9
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePlaylist(int id)
+        {
+            var user = await _userManager.FindByNameAsync(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var playlist = _context.Playlists.Where(p => p.ApplicationUser.Id == user.Id && p.Id == id).FirstOrDefault();
+
+            if (playlist == null)
+            {
+                return NotFound();
+            }
+
+            _context.Playlists.Remove(playlist);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
     }
 }
